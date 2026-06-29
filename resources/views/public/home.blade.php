0c80e92280d0417f7613167e86df99726dd851eb
adafi13 <adityanovaldy721@gmail.com>
2026-06-30 00:10:12 +0700

    fix: Add guestbook to public actions array to render the card

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -249,6 +249,7 @@
                 ['modal'=>'lapor-peristiwa','title'=>'Catatan Sipil Warga','desc'=>'Laporkan peristiwa kelahiran, kematian, atau warga pindah.','color'=>'#7c3aed','bg'=>'bg-purple-50','icon'=>'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'],
                 ['modal'=>'pinjam-inventaris-list','title'=>'Pinjam Inventaris RT','desc'=>'Lihat daftar inventaris barang RT yang dapat dipinjam gratis.','color'=>'#3b82f6','bg'=>'bg-blue-50','icon'=>'M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10'],
                 ['modal'=>'lapor-kos','title'=>'Lapor Kos / Kontrakan','desc'=>'Lapor data penghuni kos atau kontrakan baru di wilayah RT.','color'=>'#6366f1','bg'=>'bg-indigo-50','icon'=>'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5'],
+                ['modal'=>'guestbook','title'=>'Buku Tamu Pos Satpam','desc'=>'Khusus petugas keamanan untuk mencatat kendaraan & tamu masuk.','color'=>'#f43f5e','bg'=>'bg-rose-50','icon'=>'M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z'],
             ]; @endphp
             
             @foreach($actions as $a)
